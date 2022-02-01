5978f97bd37f8c02dd74d3dfcb07b3c5e4ab4b8a
mckaymatt3 <mckaymatt@outlook.com>
2022-02-01 11:19:57 -0500

    commit tree 3ec2807f46199d73d44d9156632fd5f55bf4e03a

---
+++ index.js
@@ -0,0 +1,16 @@
+// const receivesAFunction = (callback) => {return callback(receives2Functions = (main) => {return main});};
+
+function receivesAFunction(spy) {
+    spy();
+}
+
+function returnsANamedFunction() {
+    return function Miami() {}
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {}
+}
+
+
+
